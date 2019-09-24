170eaadcb2436818428f7520f438f1b7a3938e97
FlamingBudder <44650982+FlamingBudder@users.noreply.github.com>
2019-09-24 10:40:36 -0500

    Add files via upload

--- ai/Chapter3HW.lisp
+++ ai/Chapter3HW.lisp
@@ -25,5 +25,38 @@
 (defun play (p1 p2)
   (cond ((eq p1 p2)
          '0)
-        (or (and (eq p1 'rock) (eq p2 'scissors))
-            (and (eq p1 'scissors))
+        ((or (and (eq p1 'rock) (eq p2 'scissors))
+             (and (eq p1 'scissors) (eq p2 'paper))
+             (and (eq p1 'paper) (eq p2 'rock)))
+         '1)
+        (t '2)))
+
+(defun palindromep (list)
+  (if (equal (rev list) list)
+      t
+      nil))
+
+(defun rev (l)
+   (cond
+     ((null l) '())
+     (T (append (rev (cdr l)) (list (car l)))))) 
+
+(defun make-palindrome (list)
+  (append list (rev list)))
+
+(defun squarechance (s e)
+  (let ((c 0))
+    (loop for x from s
+          while (< (expt x 2) e)
+          do (incf c))
+    (/ c (+ 1 (- e s)))))
+
+(defun roll (n)
+  (let ((x 0))
+    (loop while (> n 0)
+          do (decf n)
+          if (= (random 6) 5)
+          do (incf x))
+    x))
+
+(defun test-game ())
