8846c53ae3346e789eaa9540ce192cf04586abac
FlamingBudder <44650982+FlamingBudder@users.noreply.github.com>
2019-09-24 20:35:58 -0500

    Add files via upload

--- ai/Chapter3HW.lisp
+++ ai/Chapter3HW.lisp
@@ -1,3 +1,4 @@
+
 (defun constrain (x min max)
   (cond ((< x min) min)
         ((> x max) max)
@@ -37,9 +38,9 @@
       nil))
 
 (defun rev (l)
-   (cond
-     ((null l) '())
-     (T (append (rev (cdr l)) (list (car l)))))) 
+  (cond
+    ((null l) '())
+    (T (append (rev (cdr l)) (list (car l)))))) 
 
 (defun make-palindrome (list)
   (append list (rev list)))
@@ -51,12 +52,61 @@
           do (incf c))
     (/ c (+ 1 (- e s)))))
 
-(defun roll (n)
-  (let ((x 0))
+(defun game1 ()
+  (let ((x 0)
+        (n 6))
     (loop while (> n 0)
           do (decf n)
           if (= (random 6) 5)
           do (incf x))
-    x))
+    (>= x 1)))
 
-(defun test-game ())
+(defun game2 ()
+  (let ((win nil)
+        (n 24))
+    (loop while (> n 0)
+          do (decf n)
+          if (= (random 6) (random 6) 5)
+          do (setf win t)
+          do (setf n 0))
+    win))
+
+(defun winrate (g n)
+  (let ((wins 0)
+        (orig n))
+    (loop while (> n 0)
+
+          do (decf n)
+          if (funcall g)
+          do (incf wins))
+    (/ wins orig)))
+
+                                        ; game1 - 66.099%
+                                        ; game2 -  2.873%
+                                        ; game 1 easier by 63.226% or ~23 times easier
+
+(defun left-side (list)
+  (loop while (not (equal (first list) '-vs-))
+        collect (first list)
+        do (setf list (rest list))))
+
+(defun right-side (list)
+  (let ((current nil))
+    (loop while (not (equal current '-vs-))
+          do (setf current (first list))
+          do (setf list (rest list)))
+    (loop while (> (length list) 0)
+          collect (first list)
+          do (setf list (rest list))))
+    )
+
+(defun count-common (l1 l2)
+  (let ((common 0))
+    (loop for x in l1
+          do (loop for y in l2
+                   if (eq x y)
+                   do (incf common)))
+    common))
+
+(defun compare (list)
+  (count-common (left-side list) (right-side list)))
