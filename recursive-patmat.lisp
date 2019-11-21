2bb59949dba73f8a0a11c5e4061da1ebd5c66c4c
FlamingBudder <44650982+FlamingBudder@users.noreply.github.com>
2019-11-21 10:40:18 -0600

    Add files via upload

--- recursive-patmat.lisp
+++ recursive-patmat.lisp
@@ -3,23 +3,36 @@
     (if (eq (elt str 0) #\?)
         (elt str 1))))
 
-(defun combine-assoc (l1 l2)
-  (loop for x in l2
-        do (setf l1 (acons (car x) (cdr x)
-                           l1)))
-  l1)
+(defun aspend (list1 list2)
+  (loop for x in list2
+        do (setf list1 (acons (car x)
+                              (cdr x)
+                              list1))))
 
 (defun pattern-match (pat mat &optional vars)
-  (cond ((and (listp pat) (listp mat))
+  (cond  ((and pat mat (listp pat) (listp mat))
          (let ((first (pattern-match (first pat) (first mat) vars))
-               (rest (pattern-match (rest pat) (rest mat) vars)))
-           (if (and first rest)
-               (combine-assoc vars (combine-assoc first rest)))))
+               (rest nil))
+           (if first
+               (setf rest (pattern-match (rest pat) (rest mat) (list first))))
+           (if rest
+               )))
         ((variablep pat)
-         (if (or (not (assoc vars (variablep pat)))
-                 (equal (assoc vars (variablep pat)) mat))
+         (if (or (not (assoc pat vars))                ;variable is not defined yet
+                 (equal (cdr (assoc pat vars)) mat))   ;value of mat matches the already defined variable
              (cons pat mat)))
         ((eq pat mat)
-         (cons T T))
+         (cons t t))
         (t nil)))
 
+(defun seg-match (pat mat &optional vars)
+  (let ((matlen (length mat))
+        (patlen (length pat))
+        (check nil))
+    (loop for x from 0 to (- matlen
+                             patlen)
+          do (setf check (pattern-match pat
+                                    (subseq mat x (+ x patlen))
+                                    vars))
+          if check
+          collect (list x check))))
